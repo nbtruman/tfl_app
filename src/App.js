70c8e743a5a06f3b17fcadc2356ac1bf65480dc0
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-07 21:24:17 +0000

    import JSON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { useState, useEffect } from "react";
 import './App.css';
+import Stations from "./resources/Stations.json"
 
 function App() {
+  const stationList = Object.values(JSON.parse(JSON.stringify(Stations)));
+  console.log(stationList[135].Name);
+
+
   return (
     <div className="App">
-
+      <h1>TfL App</h1>
+      <p></p>
     </div>
   );
 }
