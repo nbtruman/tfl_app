336c433f4c720c33cfc607e14dfcf82ca4daabeb
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-11 18:10:11 +0000

    add some comments

--- src/components/directRoute.js
+++ src/components/directRoute.js
@@ -3,11 +3,9 @@ import {lineColours} from "../resources/lineColours";
 
 export function DirectRoutes(props) {
 
-    // display the line with a css class property equal to the string value of the line. The css classes will be stored in an object that can be imported in.
-    // display the bus routes.
     const [ tubeLines, setTubeLines ] = useState([]);
     const [ busRoutes, setBusRoutes ] = useState([]);
-
+    // splits the directRoutes prop array into tube lines and bus routes when the component mounts.
     useEffect(() => {
         setTubeLines(props.directRoutes.filter(item => item.length >= 6));
         setBusRoutes(props.directRoutes.filter(item => item.length < 6));
@@ -16,7 +14,8 @@ export function DirectRoutes(props) {
     if(props.directRoutes.length){
         return(
         <>
-            {tubeLines.map((line, index) => <div style={{backgroundColor: `${lineColours[line]}`}}>{`${line}`}</div>)}
+            {/* maps the tube lines to individual divs and displays each one with the correct colour, imported from the lineColours object. */}
+            {tubeLines.map((line, index) => <div key ={index} style={{backgroundColor: `${lineColours[line]}`}}>{`${line}`}</div>)}
             <div>{`The following direct bus routes are also available: ${busRoutes}`}</div>
         </>)
     }else {
