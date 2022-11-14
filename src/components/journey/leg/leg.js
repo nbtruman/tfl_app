bdc64c158a98560c9c4763dd93eed7b1db0513a3
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-14 14:12:15 +0000

    change leg class to line-p

--- src/components/journey/leg/leg.js
+++ src/components/journey/leg/leg.js
@@ -3,7 +3,7 @@ import { lineColours } from "../../../resources/lineColours";
 export function Leg(props){
     return (
         <div className="line-div" style={{backgroundColor: `${lineColours[props.information.routeOptions[0].name.toLowerCase()]}`}}>
-            <p className="leg">{`${props.information.instruction.summary}`}</p>
+            <p className="line-p">{`${props.information.instruction.summary}`}</p>
         </div>
     )
 }
