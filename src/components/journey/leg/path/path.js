bea796f2cbe8452f9f388d481c2c9fcd9ab35c16
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-15 23:15:17 +0000

    add style

--- src/components/journey/leg/path/path.js
+++ src/components/journey/leg/path/path.js
@@ -2,8 +2,8 @@ import "./path.css";
 
 export function Path(props) {
     return (
-        <div>
-            {props.pathArray ? props.pathArray.map((e, i) => <p key={i} className="stop-point">{e.name.slice(0, -19)}</p>) : <p>I'm a path</p>}
+        <div className="path">
+            {props.pathArray ? props.pathArray.map((e, i) => <p key={i} className="stop-point">{e.name}</p>) : <p>I'm a path</p>}
         </div>
     )
 }
