a144815027420ab469078ccc19cc59c2dedb9fd4
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-16 13:10:34 +0000

    add click event

--- src/components/journey/leg/leg.js
+++ src/components/journey/leg/leg.js
@@ -5,15 +5,22 @@ import "./leg.css"
 
 export function Leg(props){
     const [ path, setPath ] = useState();
+    const [ hidden, setHidden ] = useState(true)
 
     useEffect(() => {
         setPath(props.information.path.stopPoints)
     }, [props.information.path.stopPoints])
 
+    const toggleHidden = () => {
+        setHidden(!hidden);
+    }
+
     return (
-        <div className="line-div" style={{backgroundColor: `${lineColours[props.information.routeOptions[0].name.toLowerCase()]}`}}>
+        <div    className="line-div" 
+                style={{backgroundColor: `${lineColours[props.information.routeOptions[0].name.toLowerCase()]}`}}
+                onClick={toggleHidden}>
             <p className="line-p">{`${props.information.instruction.summary}`}</p>
-            <div>
+            <div className={hidden ? "hideDetail" : "showDetail"}>
                 <Path pathArray={path}/>
             </div>            
         </div>
