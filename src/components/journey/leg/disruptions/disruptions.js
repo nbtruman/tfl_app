e0fc957f040b18e90a13c8d5c601096a2383caa4
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-16 16:05:26 +0000

    add disruptions

--- src/components/journey/leg/disruptions/disruptions.js
+++ src/components/journey/leg/disruptions/disruptions.js
@@ -3,7 +3,7 @@ import "./disruptions.css";
 export function Disruptions(props) {
     return (
         <div className="disruption">
-            {props.disruptionArray ? props.disruptionArray.map((e, i) => <p key={i}>{e.description}</p>) : <p></p>}
+            {props.disruptionArray ? props.disruptionArray.map((e, i) => <p className="alert" key={i}>{e.description}</p>) : <p></p>}
         </div>
     )
 }
