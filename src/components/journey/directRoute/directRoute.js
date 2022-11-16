9ff0e8c8ba3ea205a9b25d6163746008f581a20f
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-11-16 13:50:04 +0000

    add .length to direct routes

--- src/components/journey/directRoute/directRoute.js
+++ src/components/journey/directRoute/directRoute.js
@@ -9,7 +9,7 @@ export function DirectRoutes(props) {
         setLine(props.routes[0].routeOptions[0].name);
     }, [props.routes])
 
-    if(props.routes === 1){
+    if(props.routes.length === 1){
         return <p className="text-block">There is a direct route available on the <span>{line}</span> line.</p>
     }else{
         return <p className="text-block">Your journey has {props.routes.length - 1} change{props.routes.length === 2 ? "" : "s"}</p>
